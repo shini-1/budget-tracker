d526601538af15f64b611d4c75e49ff4accd7d6c
shini-1 <ginoy26@gmail.com>
2026-01-28 15:12:13 +0800

    Remove unused ProtectedRoute import

--- Desktop/budget-tracker/client/src/App.js
+++ Desktop/budget-tracker/client/src/App.js
@@ -3,7 +3,6 @@ import { AuthProvider, useAuth } from './contexts/AuthContext';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
-import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
 
 function AppContent() {
